ad79dd75601046bd68f4340992274556eb731845
SebastianCalderonM <74921708+SebastianCalderonM@users.noreply.github.com>
2021-12-14 01:10:34 -0300

    let´s GO

--- Lab3_SD/Informante_client/main.go
+++ Lab3_SD/Informante_client/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	pb "github.com/CodeZeo/T3-SD/Lab3_SD/comms"
 	"google.golang.org/grpc"
@@ -20,6 +21,7 @@ var (
 	name = flag.String("name", defaultName, "Name to greet")
 )
 
+//Función que realiza consultas al Broker para obtener la dirección de algún Fulcrum al que conectarse a realiza el add
 func agCity(Data *pb.DataCity) pb.Clock {
 	//Consultar por la ip de un Server Fulcrum
 	var conn *grpc.ClientConn
@@ -32,7 +34,7 @@ func agCity(Data *pb.DataCity) pb.Clock {
 	}
 	defer conn.Close()
 	cc := pb.NewBrokerClient(conn)
-	comando := "AddCity " + Data.NombrePlaneta + " " + Data.NombreCiudad
+	comando := "AddCity " + Data.NombrePlaneta + " " + Data.NombreCiudad + " " + strconv.Itoa(int(Data.NuevoValor))
 	response, err := cc.GetIP(context.Background(), &pb.Command{C: comando})
 	//conectar al Fulcrum
 	conn, err = grpc.Dial(response.Ip+":9005", grpc.WithInsecure())
@@ -47,6 +49,96 @@ func agCity(Data *pb.DataCity) pb.Clock {
 	return pb.Clock{X: int32(reloj.X), Y: int32(reloj.Y), Z: int32(reloj.Z)}
 }
 
+//Función que realiza consultas al Broker para obtener la dirección de algún Fulcrum al que conectarse a realiza el update
+func upNCity(Data *pb.ChangeNameCity) pb.Clock {
+	//Consultar por la ip de un Server Fulcrum
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("localhost:9003", grpc.WithInsecure())
+	if err != nil {
+		conn, err = grpc.Dial("localhost:9004", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("did not connect: %s", err)
+		}
+	}
+	defer conn.Close()
+	cc := pb.NewBrokerClient(conn)
+	comando := "UpdateName " + Data.NombrePlaneta + " " + Data.NombreCiudad + " " + Data.NuevoNombre
+	response, err := cc.GetIP(context.Background(), &pb.Command{C: comando})
+	//conectar al Fulcrum
+	conn, err = grpc.Dial(response.Ip+":9005", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+	ccc := pb.NewFulcrumClient(conn)
+	//realizar Create
+	reloj, err := ccc.UpdateName(context.Background(), &pb.ChangeNameCity{})
+	if err != nil {
+		log.Fatalf("did not update: %s", err)
+	}
+	return pb.Clock{X: int32(reloj.X), Y: int32(reloj.Y), Z: int32(reloj.Z)}
+}
+
+//Función que realiza consultas al Broker para obtener la dirección de algún Fulcrum al que conectarse a realiza el update
+func upVCity(Data *pb.DataCity) pb.Clock {
+	//Consultar por la ip de un Server Fulcrum
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("localhost:9003", grpc.WithInsecure())
+	if err != nil {
+		conn, err = grpc.Dial("localhost:9004", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("did not connect: %s", err)
+		}
+	}
+	defer conn.Close()
+	cc := pb.NewBrokerClient(conn)
+	comando := "UpdateNumber" + Data.NombrePlaneta + " " + Data.NombreCiudad + " " + strconv.Itoa(int(Data.NuevoValor))
+	response, err := cc.GetIP(context.Background(), &pb.Command{C: comando})
+	//conectar al Fulcrum
+	conn, err = grpc.Dial(response.Ip+":9005", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+	ccc := pb.NewFulcrumClient(conn)
+	//realizar Create
+	reloj, err := ccc.UpdateNumber(context.Background(), &pb.DataCity{})
+	if err != nil {
+		log.Fatalf("did not update: %s", err)
+	}
+	return pb.Clock{X: int32(reloj.X), Y: int32(reloj.Y), Z: int32(reloj.Z)}
+}
+
+//Función que realiza consultas al Broker para obtener la dirección de algún Fulcrum al que conectarse a realiza el delete
+func DeleteC(Data *pb.LocateCity) pb.Clock {
+	//Consultar por la ip de un Server Fulcrum
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("localhost:9003", grpc.WithInsecure())
+	if err != nil {
+		conn, err = grpc.Dial("localhost:9004", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("did not connect: %s", err)
+		}
+	}
+	defer conn.Close()
+	cc := pb.NewBrokerClient(conn)
+	comando := "DeleteCity" + Data.NombrePlaneta + " " + Data.NombreCiudad
+	response, err := cc.GetIP(context.Background(), &pb.Command{C: comando})
+	//conectar al Fulcrum
+	conn, err = grpc.Dial(response.Ip+":9005", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+	ccc := pb.NewFulcrumClient(conn)
+	//realizar Create
+	reloj, err := ccc.DeleteCity(context.Background(), &pb.LocateCity{})
+	if err != nil {
+		log.Fatalf("did not delete: %s", err)
+	}
+	return pb.Clock{X: int32(reloj.X), Y: int32(reloj.Y), Z: int32(reloj.Z)}
+}
+
 func gnr() {
 	flag.Parse()
 	// Set up a connection to the server.
@@ -56,23 +148,46 @@ func gnr() {
 	}
 	defer conn.Close()
 	//c := pb.NewBrokerClient(conn)
+	var linea string
 
-	var planeta string
-	var ciudad string
+	fmt.Println("Ingrese el comando: ")
+	fmt.Scanln(&linea)
 
-	fmt.Println("Ingrese el planeta a buscar: ")
-	fmt.Scanln(&planeta)
-
-	fmt.Println("Ingrese la ciudad a buscar: ")
-	fmt.Scanln(&ciudad)
+	s := strings.Fields(linea)
+	var reloj pb.Clock
+	if s[0] == "AddCity" {
+		if len(s) == 4 {
+			numero, err := strconv.Atoi(s[3])
+			if err != nil {
+				fmt.Println("NuevoValor invalido: ", err)
+			} else {
+				reloj = agCity(&pb.DataCity{NombrePlaneta: s[1], NombreCiudad: s[2], NuevoValor: int32(numero)})
+			}
+		} else {
+			reloj = agCity(&pb.DataCity{NombrePlaneta: s[1], NombreCiudad: s[2], NuevoValor: 0})
+		}
+	} else if s[0] == "UpdateName" {
+		reloj = upNCity(&pb.ChangeNameCity{NombrePlaneta: s[1], NombreCiudad: s[2], NuevoNombre: s[3]})
+	} else if s[0] == "UpdateNumber" {
+		numero, err := strconv.Atoi(s[3])
+		if err != nil {
+			fmt.Println("NuevoValor invalido: ", err)
+		} else {
+			reloj = upVCity(&pb.DataCity{NombrePlaneta: s[1], NombreCiudad: s[2], NuevoValor: int32(numero)})
+		}
+	} else if s[0] == "DeleteCity" {
+		reloj = DeleteC(&pb.LocateCity{NombrePlaneta: s[1], NombreCiudad: s[2]})
+	} else {
+		fmt.Println("Comando Invalido.")
+	}
 	// Contact the server and print out its response.
 	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	//defer cancel()
-	r := agCity(&pb.DataCity{NombrePlaneta: planeta, NombreCiudad: ciudad, NuevoValor: 0})
+	//r := agCity(&pb.DataCity{NombrePlaneta: planeta, NombreCiudad: ciudad, NuevoValor: 0})
 	//if err != nil {
 	//	log.Fatalf("could not greet: %v", err)
 	//}
-	log.Printf("El reloj es:[ %s,%s,%s ]", strconv.Itoa(int(r.X)), strconv.Itoa(int(r.Y)), strconv.Itoa(int(r.Z)))
+	log.Printf("El reloj es:[ %s,%s,%s ]", strconv.Itoa(int(reloj.X)), strconv.Itoa(int(reloj.Y)), strconv.Itoa(int(reloj.Z)))
 }
 
 func main() {
@@ -81,7 +196,7 @@ func main() {
 	var opcion int
 	for flag {
 		fmt.Println("Ingrese una opcion: ")
-		fmt.Println("1) Ver rebeldes en ciudad.")
+		fmt.Println("1) Ingresar Comando")
 		fmt.Println("2) Salir.")
 		fmt.Scanln(&opcion)
 		if opcion == 1 {
